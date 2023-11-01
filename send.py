5b1f76d87ef89cef899e8435d3f60c2b4144ade4
VIMAH <21022208@student.hhs.nl>
2023-11-01 16:02:46 +0100

    ander apparaat
    kon niet vanaf nuc, vandaar via eigen laptop

--- send.py
+++ send.py
@@ -1,63 +1,39 @@
-# ------ Hieronder staat de uitwerking van RabbitMQ docs
-
-# #!/usr/bin/env python
-# import pika
-
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='localhost'))
-# channel = connection.channel()
-
-# channel.queue_declare(queue='hello')
-
-# channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-# print(" [x] Sent 'Hello World!'")
-# connection.close() 
-
-# ------ Hieronder staat de uitwerking van een JSON-Object versturen 
-
-#!/usr/bin/env python
 import pika
 import json
+from datetime import datetime
 
-# # Maak een Python-dictionary om te converteren naar JSON
-# message_data = {
-#     'name': 'John Doe',
-#     'email': 'johndoe@example.com',
-#     'age': 30
-# }
+def publish_message(channel, message_data):
+    # Handmatig omzetten van datetime naar ISO 8601-string
+    message_data["time"] = message_data["time"].isoformat()
 
-# # Converteer het dictionary naar een JSON-string
-# message_json = json.dumps(message_data)
+    message = json.dumps(message_data)
+    channel.basic_publish(
+        exchange='spring-boot-exchange',
+        routing_key='key.cfns.measurement',
+        body=message,
+        properties=pika.BasicProperties(
+            content_type="application/json",
+            headers={'TypeId': 'nl.cfns.entity.Measurement'},
+            content_encoding='UTF-8',
+            delivery_mode=2,  # maak het bericht persistent
+        )
+    )
+    print("Sent", message)
 
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='localhost'))
-# channel = connection.channel()
-
-# channel.queue_declare(queue='hello')
-
-# # Verzend het JSON-object als het berichtlichaam
-# channel.basic_publish(exchange='', routing_key='hello', body=message_json)
-# print(f" [x] Sent JSON: {message_json}")
-# connection.close()
-
-# Maak een Python-dictionary om te converteren naar JSON
 message_data = {
-  "id": 2003,
-  "time": "2023-11-01 14:00:00.05",
-  "latency": 1000,
-  "upload": 2000,
-  "download": 5000,
-  "RSSI": -20,
-  "RSRQ": 1,
-  "RSRP": -100,
-  "SINR": 4,
-  "mnoString": "KPN",
-  "latitude": 51.98527662447251,
-  "longitude": 4.3455394836358305
+    "id": 2003,
+    "time": datetime(2023, 11, 1, 14, 0, 0, 50000),  # Vervang dit met jouw datetime-object
+    "latency": 1000,
+    "upload": 2000,
+    "download": 5000,
+    "RSSI": -20,
+    "RSRQ": 1,
+    "RSRP": -100,
+    "SINR": 4,
+    "mnoString": "KPN",
+    "latitude": 51.985276,
+    "longitude": 4.345539
 }
-
-# Converteer het dictionary naar een JSON-string
-message_json = json.dumps(message_data)
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -65,7 +41,10 @@ channel = connection.channel()
 
 channel.queue_declare(queue='key.cfns.measurement')
 
-# Verzend het JSON-object als het berichtlichaam
-channel.basic_publish(exchange='spring-boot-exchange', routing_key='key.cfns.measurement', body=message_json)
-print(f" [x] Sent JSON: {message_json}")
-connection.close()
+try:
+    publish_message(channel, message_data)
+except Exception as e:
+    print("An error occurred:", str(e))
+finally:
+    if connection.is_open:
+        connection.close()
