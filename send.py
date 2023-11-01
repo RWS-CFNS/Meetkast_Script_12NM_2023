55bbf921fded8680b349b0bd6cceed3d5e42c69a
Vinay <21022208@student.hhs.nl>
2023-11-01 14:19:41 +0100

    naar back end sturen
    - juiste info
    - juiste formaat
    - komt aan in back en, alleen kan nog niet. lijkt te wijzen op een probleem met de deserialisatie van een JSON-object uit een bericht in RabbitMQ. Om de fout op te lossen, moet je ervoor zorgen dat de ontvangende kant het JSON-object correct deserialiseert.

--- send.py
+++ send.py
@@ -19,11 +19,41 @@
 import pika
 import json
 
+# # Maak een Python-dictionary om te converteren naar JSON
+# message_data = {
+#     'name': 'John Doe',
+#     'email': 'johndoe@example.com',
+#     'age': 30
+# }
+
+# # Converteer het dictionary naar een JSON-string
+# message_json = json.dumps(message_data)
+
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
+
+# channel.queue_declare(queue='hello')
+
+# # Verzend het JSON-object als het berichtlichaam
+# channel.basic_publish(exchange='', routing_key='hello', body=message_json)
+# print(f" [x] Sent JSON: {message_json}")
+# connection.close()
+
 # Maak een Python-dictionary om te converteren naar JSON
 message_data = {
-    'name': 'John Doe',
-    'email': 'johndoe@example.com',
-    'age': 30
+  "id": 2003,
+  "time": "2023-11-01 14:00:00.05",
+  "latency": 1000,
+  "upload": 2000,
+  "download": 5000,
+  "RSSI": -20,
+  "RSRQ": 1,
+  "RSRP": -100,
+  "SINR": 4,
+  "mnoString": "KPN",
+  "latitude": 51.98527662447251,
+  "longitude": 4.3455394836358305
 }
 
 # Converteer het dictionary naar een JSON-string
@@ -33,9 +63,9 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='key.cfns.measurement')
 
 # Verzend het JSON-object als het berichtlichaam
-channel.basic_publish(exchange='', routing_key='hello', body=message_json)
+channel.basic_publish(exchange='spring-boot-exchange', routing_key='key.cfns.measurement', body=message_json)
 print(f" [x] Sent JSON: {message_json}")
 connection.close()
