c85ec0ac783d54be244ad8fb1617c259c9e60cc5
VIMAH <21022208@student.hhs.nl>
2023-11-09 14:41:36 +0100

    update
    basis vereisten checken

--- send.py
+++ send.py
@@ -2,21 +2,9 @@ import pika
 import json
 from datetime import datetime
 
-def publish_message(channel, message_data):
-    message = json.dumps(message_data)
-    channel.basic_publish(
-        exchange='spring-boot-exchange',
-        routing_key='key.cfns.measurement',
-        body=message,
-        properties=pika.BasicProperties(
-            content_type="application/json",
-            content_encoding='UTF-8',
-            headers={'TypeId': 'nl.cfns.entity.Measurement'},
-            delivery_mode=2,  # maak het bericht persistent
-            priority=0
-        )
-    )
-    print("Sent", message)
+# Maak een verbinding met de RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
 
 # Huidige datetime verkrijgen
 current_time = datetime.now()
@@ -24,7 +12,9 @@ current_time = datetime.now()
 # Convert the datetime object to an ISO 8601 formatted string
 current_time_iso = current_time.isoformat()
 
-message_data = {
+
+# Maak een dictionary met je data
+data = {
     "id": 2003,
     "time": current_time_iso,  # Gebruik het ISO 8601-formatted string
     "latency": 1000,
@@ -39,16 +29,29 @@ message_data = {
     "longitude": 4.345539
 }
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+# Converteer de dictionary naar een JSON string
+message = json.dumps(data)
 
-channel.queue_declare(queue='key.cfns.measurement')
+# print(message)
 
-try:
-    publish_message(channel, message_data)
-except Exception as e:
-    print("An error occurred:", str(e))
-finally:
-    if connection.is_open:
-        connection.close()
+# Stel de berichteigenschappen in
+properties = pika.BasicProperties(
+    content_type="application/json",
+    content_encoding='UTF-8',
+    headers={'_TypeId_': 'nl.cfns.entity.Measurement'},
+    delivery_mode=2,  # maak het bericht persistent
+    priority=0
+)
+
+# Stuur het bericht
+channel.basic_publish(
+    exchange='spring-boot-exchange',
+    routing_key='key.cfns.measurement',
+    body=message,
+    properties=properties
+)
+
+print(" [x] Sent %r" % message)
+
+# Sluit de verbinding
+connection.close()
