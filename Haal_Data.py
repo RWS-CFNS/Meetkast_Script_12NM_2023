fc1de983c3541309fa83bb9d0c4daf6b493dbe3d
Vinay <101119097+VIMAH@users.noreply.github.com>
2023-12-22 20:56:42 +0100

    Update Haal_Data.py
    haal onnodige comments/oude code weg

--- Haal_Data.py
+++ Haal_Data.py
@@ -1,34 +1,3 @@
-# import psycopg2
-
-# # Verbinding maken met de database
-# conn = psycopg2.connect(
-#     dbname="test_db",
-#     user="postgres",
-#     password="stagecfns",
-#     host="127.0.0.1"  # of het IP-adres van de database
-# )
-
-# # Cursor maken om query's uit te voeren
-# cur = conn.cursor()
-
-# # Voorbeeldquery om data op te halen
-# # query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'personen'"
-# query = "SELECT * FROM personen.persoonsgegevens"
-
-# # Uitvoeren van de query
-# cur.execute(query)
-
-# # Alle rijen ophalen
-# rows = cur.fetchall()
-
-# # Resultaten tonen
-# for row in rows:
-#     print(row)
-
-# # Verbinding sluiten
-# cur.close()
-# conn.close()
-
 import psycopg2
 
 # Verbinding maken met de database
