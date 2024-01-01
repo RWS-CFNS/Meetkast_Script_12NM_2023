2df33637420aed1e84a646d12c69c6fbfea4fe03
Vinay <101119097+VIMAH@users.noreply.github.com>
2024-01-01 17:21:59 +0100

    Update and rename Main.py to main.py
    pylint main.py
    --------------------------------------------------------------------
    Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Module to handle measurement data retrieval and transmission.
+"""
+
+import time
+from datetime import datetime
+import subprocess
+
+def run_script(script_name):
+    """
+    Voert het opgegeven script uit met behulp van Python 3 subprocess.run.
+    """
+    try:
+        subprocess.run(["python3", script_name], check=True)
+        print(f"{script_name} is succesvol uitgevoerd")
+    except subprocess.CalledProcessError as e:
+        print(f"Fout bij het uitvoeren van {script_name}: {e}")
+
+def main():
+    """
+    Voert metingsscripts uit op een bepaald interval.
+    """
+    # Aantal seconden in 15 minuten
+    interval_seconds = 15 * 60
+
+    while True:
+        print(f"Uitvoeren van metingsscripts op: {datetime.now()}")
+        # Voer de verschillende scripts uit
+        run_script("measurement.py")
+        run_script("measuringbox.py")
+        run_script("weather_measurement.py")
+        # Wacht 15 minuten voordat de scripts opnieuw worden uitgevoerd
+        time.sleep(interval_seconds)
+
+if __name__ == "__main__":
+    main()
