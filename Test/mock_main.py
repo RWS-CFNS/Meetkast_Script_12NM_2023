300c26a94b2a6e64b846b97cc9bb1dbf576cf1c5
Vinay <101119097+VIMAH@users.noreply.github.com>
2024-01-07 14:23:14 +0100

    Update mock_main.py
    custom_sleep toevoegen zodat er tijdens test time.sleep en time goed omgegaan wordt

--- Test/mock_main.py
+++ Test/mock_main.py
@@ -1,9 +1,3 @@
-"""
-Module to handle measurement data retrieval and transmission.
-"""
-
-import time
-from datetime import datetime
 import subprocess
 
 def run_script(script_name):
@@ -16,21 +10,31 @@ def run_script(script_name):
     except subprocess.CalledProcessError as e:
         print(f"Fout bij het uitvoeren van {script_name}: {e}")
 
+MAX_ITERATIONS = 3  # Stel het maximale aantal iteraties in
+
+def custom_sleep(seconds):
+    pass  # Hier wordt niets gedaan, zodat de functie geen tijd wacht
+
 def main():
     """
     Voert metingsscripts uit op een bepaald interval.
     """
     # Aantal seconden in 15 minuten
     interval_seconds = 15 * 60
+    iterations = 0  # Initialisatie van de iteratieteller
 
-    while True:
-        print(f"Uitvoeren van metingsscripts op: {datetime.now()}")
+    while iterations < MAX_ITERATIONS:
+        # print(f"Uitvoeren van metingsscripts op: {datetime.now()}")
         # Voer de verschillende scripts uit
         run_script("measurement.py")
         run_script("measuringbox.py")
         run_script("weather_measurement.py")
         # Wacht 15 minuten voordat de scripts opnieuw worden uitgevoerd
-        time.sleep(interval_seconds)
+        custom_sleep(interval_seconds)  # Gebruik de aangepaste sleep functie
+        iterations += 1  # Verhoog de iteratieteller
+
+    # Verwijder het exit statement na het beëindigen van de lus
+    # print("Maximale iteraties bereikt. Het script wordt beëindigd.")
 
 if __name__ == "__main__":
     main()
